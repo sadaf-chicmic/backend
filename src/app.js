705adafc79ec995c504599d2d2c8027630f6f41b
sadaf-chicmic <sadaf@chicmicstudios.in>
2026-07-03 16:33:44 +0530

    feat: integrate Swagger API documentation using swagger-jsdoc and swagger-ui-express

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./config/swagger");
 
 const app=express();
 app.use(express.json());
+app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(swaggerSpec));
 
 const studentRoutes = require("./routes/user.routes");
 app.use("/students",studentRoutes);
